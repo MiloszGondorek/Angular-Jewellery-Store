399606396ed391076c26f2aa3ad1b5df057667eb
MiloszGondorek <101429391+MiloszGondorek@users.noreply.github.com>
2024-01-16 20:49:01 +0100

    .

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { ProsComponent } from './pros/pros.component';
 import { RecommendedComponent } from './recommended/recommended.component';
 import { ButtonComponent } from '../reusable/button/button.component';
+import { PresentLinkComponent } from './present-link/present-link.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProsComponent, RecommendedComponent, ButtonComponent],
+  imports: [
+    ProsComponent,
+    RecommendedComponent,
+    ButtonComponent,
+    PresentLinkComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- frontend/src/app/home/present-link/present-link.component.ts
+++ frontend/src/app/home/present-link/present-link.component.ts
@@ -2,15 +2,15 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'recommended',
+  selector: 'present-link',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './recommended.component.html',
-  styleUrl: './recommended.component.scss',
+  templateUrl: './present-link.component.html',
+  styleUrl: './present-link.component.scss',
 })
-export class RecommendedComponent {
-  @Input() src = '';
-  @Input() isLarge = true;
-  @Input() header = '';
+export class PresentLinkComponent {
   @Input() link = '';
+  @Input() src = '';
+  @Input() header = '';
+  @Input() isLarge=true;
 }
