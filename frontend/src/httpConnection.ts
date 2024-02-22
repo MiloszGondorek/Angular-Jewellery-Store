3da79f257bbe5bc3406d1589e53f8ede34ee2ab0
MiloszGondorek <101429391+MiloszGondorek@users.noreply.github.com>
2024-02-22 15:24:23 +0100

    .

--- frontend/src/httpConnection.ts
+++ frontend/src/httpConnection.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 
 export class http {
-  static apiUrl = 'http://192.168.0.164:1337';
+  static apiUrl = 'http://YOUR_IP:1337';
   static async getData(question: string): Promise<any[]> {
     try {
       const response: AxiosResponse = await axios.get(
@@ -11,7 +11,7 @@ export class http {
       return data.data;
     } catch (error) {
       console.error('Error fetching data:', error);
-      return []; // lub możesz rzucić wyjątek, zależnie od wymagań aplikacji
+      return [];
     }
   }
   static getURL() {
